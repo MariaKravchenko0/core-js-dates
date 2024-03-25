46ea550a477b8dea2aa91ff228fb8775c976a2bc
Mariia Kravchenko <kravchenkomaria00@gmail.com>
2024-03-25 22:36:18 +0200

    feat: implement getNextFriday function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -67,8 +67,14 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const currDayOfWeek = date.getDay();
+  let daysTillFriday = (5 - currDayOfWeek + 7) % 7;
+  if (daysTillFriday === 0) daysTillFriday = 7;
+
+  const nextFriday = new Date(date);
+  nextFriday.setDate(nextFriday.getDate() + daysTillFriday);
+  return nextFriday;
 }
 
 /**
