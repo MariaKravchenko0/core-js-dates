8185c7ad25aa64aca9eced27e237c5d6b60fcb84
Mariia Kravchenko <kravchenkomaria00@gmail.com>
2024-03-27 23:02:14 +0200

    feat: implement getNextFridayThe13th function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -217,8 +217,14 @@ function getWeekNumberByDate(/* date */) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  let nextFriday = getNextFriday(date);
+
+  while (nextFriday.getDate() !== 13) {
+    nextFriday = getNextFriday(nextFriday);
+  }
+
+  return nextFriday;
 }
 
 /**
